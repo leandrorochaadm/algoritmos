aa9ae4fb62ced1e6d707328416fd089581e179f7
leandrorochaadm <leandrorochaadm@gmail.com>
2020-08-08 19:43:26 -0400

    usario inserindo tamanho da progressao

--- 001 - progressao/src/com/leandrorocha/Principal.java
+++ 001 - progressao/src/com/leandrorocha/Principal.java
@@ -1,21 +1,31 @@
 package com.leandrorocha;
 
+import java.util.Scanner;
+
+
 public class Principal {
 
+	private static final Scanner SCANNER = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		/* System.out.println("Progressao Aritmetica");
-		var ari = new ProgressaoAritmetica(2);
-		ari.imprimirProgressao(5);
-		*/
+		System.out.println("Digite o tamanho da progressão: ");
+		final int i = SCANNER.nextInt();
 		
-		/*System.out.println("Progressao Geometrica");
-		var geo = new ProgressaoGeometrica(5);
-		geo.imprimirProgressao(5);
-		*/
+		System.out.println("Progressao Aritmetica");
+		var ari = new ProgressaoAritmetica();
+		ari.imprimirProgressao(i);
 		
-		System.out.println("Progressao Fibonacci");
+		
+		System.out.println("\nProgressao Geometrica");
+		var geo = new ProgressaoGeometrica();
+		geo.imprimirProgressao(i);
+		
+		
+		System.out.println("\nProgressao Fibonacci");
 		var fib =  new ProgressaoFibonacci(1,2);
-		fib.imprimirProgressao(10);
+		fib.imprimirProgressao(i);
+		
+		SCANNER.close();
 	}
 
 }
