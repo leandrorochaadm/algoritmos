31902071464065af91ec26f4889d41c4e3113886
leandrorochaadm <leandrorochaadm@gmail.com>
2020-08-08 16:55:41 -0400

    criacao classe progressao

---
+++ 001 - progressao/src/com/leandrorocha/Progressao.java
@@ -0,0 +1,27 @@
+package com.leandrorocha;
+
+public class Progressao {
+
+	protected long valorInicial;
+	protected long valorAtual;
+	
+	Progressao(){
+		valorAtual=valorInicial=0;
+	}
+	
+	protected long primeiroValor() {
+		valorAtual = valorInicial;
+		return valorAtual;
+	}
+	 protected long proximoValor() {
+		 return ++valorInicial;
+	 }
+	 
+	 public void imprimirProgressao(int n) {
+		System.out.println(primeiroValor()); 
+		for(int i = 2; i<=n;i++) {
+			System.out.println(" "+proximoValor());
+			System.out.println(); //pula linha
+		}
+	 }
+}
